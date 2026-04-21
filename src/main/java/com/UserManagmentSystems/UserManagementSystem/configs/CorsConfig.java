1b3c33bc16c31efd78db0cd539ec918870db4607
Muhammad Hassan Ghauri <hassanghauri14@gmail.com>
2026-04-21 22:27:45 +0500

    allowed origins updated

--- src/main/java/com/UserManagmentSystems/UserManagementSystem/configs/CorsConfig.java
+++ src/main/java/com/UserManagmentSystems/UserManagementSystem/configs/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200","https://user-management-system-angular-fron.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
